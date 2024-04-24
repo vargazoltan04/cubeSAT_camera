9c8fc041558d74b832ead867e6140d822cd1201f
tudhatnad <zoltan20040605@gmail.com>
2024-04-24 19:01:19 +0200

    blabla

--- mcu_kod/mcu_kod.ino
+++ mcu_kod/mcu_kod.ino
@@ -1,23 +1,47 @@
-#include <stdio.h> 
-#include "Arduino.h"
-//#include <opencv2/opencv.hpp>
+#include <Wire.h>
 
-using namespace std;
+#define SDA_PIN 20 // Change this to the desired SDA pin
+#define SCL_PIN 21 // Change this to the desired SCL pin
 
-const uint16_t BUFFER_SIZE = 57600;
-uint8_t buffer[BUFFER_SIZE];
-void setup()
-{
-  Serial.begin(115200);
-  digitalWrite(25, HIGH);
+void setup() { 
+  Wire.begin();  // Initialize I2C communication with custom pins
+  Serial.begin(9600);  // Initialize serial communication
+  while (!Serial);  // Wait for serial port to connect
+  Serial.println("\nI2C Scanner");
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    int bytesRead = Serial.readBytes(buffer, BUFFER_SIZE);
-    
-    Serial.write(buffer, bytesRead);
+void loop() {
+  byte error, address;
+  int devices = 0;
+
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println(" !");
+      devices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
   }
+
+  if (devices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("Done\n");
+
+  delay(5000);  // Wait 5 seconds for the next scan
 }
