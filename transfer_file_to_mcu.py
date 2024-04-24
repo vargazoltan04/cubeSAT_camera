abaf4266891daad8086cc97ab9fa8692da9ddb9a
RobertNagy299 <nagyrobertoffical@gmail.com>
2024-04-24 20:38:35 +0200

    asd

--- transfer_file_to_mcu.py
+++ transfer_file_to_mcu.py
@@ -1,21 +1,23 @@
-import serial
+# import serial COMMENTED OUT FOR TESTING
 from PIL import Image
 import sys
 
-# Open the serial port
-ser = serial.Serial('COM3', 115200)
+'''
 
-data = Image.open('earth.bmp')
+# Open the serial port
+# ser = serial.Serial('COM3', 115200) COMMENTED OUT FOR TESTING
+imageSize = 12
+data = Image.open('cubeSAT_camera\earth.bmp')
 saved_mode = data.mode
 saved_size = data.size
 # Read the file and send its contents
-
-ser.write(data.tobytes()[0:57600:1])
+bytesOfImg = data.tobytes()
+# ser.write(data.tobytes()[0:57600:1]) COMMENTED OUT FOR TESTING
 i = 0    
 index = 57600
 try:
-    while i < 12:
-        tempData = ser.read(57600) 
+    while i < imageSize:
+        tempData = bytesOfImg[index-57600:index]
             
         title = f"{i}.bmp"
         
@@ -24,14 +26,37 @@ try:
         im.save(title, format='BMP')
         
         i = i + 1
-        ser.write(data.tobytes()[index:index+57600:1])
+        # ser.write(data.tobytes()[index:index+57600:1]) COMMENTED OUT FOR TESTING
         index += 57600
 
 
         
 except KeyboardInterrupt:
     # Close the serial port when the script is interrupted
-    ser.close()
+   # ser.close() COMMENTED OUT FOR TESTING
     print("Serial port closed.")
 
+'''
 
+try:
+    j = 0
+    bytesOfImg = bytes()
+    print("Elindult megint")
+    while j < 12:
+    
+       
+        title = f"{j}.bmp"
+        print(title)
+        data = Image.open(title)
+    
+        saved_mode = data.mode
+        saved_size = data.size
+        bytesOfImg += (data.tobytes())
+        j+=1
+    
+    im = Image.frombytes('RGB',(480,480), bytesOfImg)
+    name = f"pieced_together.bmp"
+    im.save(name, format='BMP')
+
+except Exception as e:
+    print(repr(e))
